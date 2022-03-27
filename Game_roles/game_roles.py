5ae0a6f7c0f4fa69ab55afd934f0888ea43e8ebe
Hunter 87 <83773586+Hunter87ff@users.noreply.github.com>
2022-03-27 11:32:33 +0530

    update game_roles.py

--- Game_roles/game_roles.py
+++ Game_roles/game_roles.py
@@ -1,1 +1,22 @@
+import discord
+from discord.ext import commands
 
+
+bot = commands.Bot(command_prefix= when_mentioned_or("@"))
+
+
+
+
+ff = "Garena Free Fire Max is a battle royal game. Played by millions of people. Developed by 111 dots studio and published by Garena. React on the emoji to access this game!"
+
+ffemb = discord.Embed(title="FREE FIRE", description=ff, color=discord.Color.blurple())
+
+
+
+
+
+
+
+@bot.command()
+async def game_roles(ctx):
+  await ctx.send(embed=ffemb)
