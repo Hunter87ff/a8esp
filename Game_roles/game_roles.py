acb5021b8c0c92250b661c1c0035b1cc3d8bc36a
Hunter 87 <83773586+Hunter87ff@users.noreply.github.com>
2022-03-27 11:41:44 +0530

    update game_roles.py

--- Game_roles/game_roles.py
+++ Game_roles/game_roles.py
@@ -10,6 +10,7 @@ bot = commands.Bot(command_prefix= when_mentioned_or("@"))
 ff = "Garena Free Fire Max is a battle royal game. Played by millions of people. Developed by 111 dots studio and published by Garena. React on the emoji to access this game!"
 
 ffemb = discord.Embed(title="FREE FIRE", description=ff, color=discord.Color.blurple())
+ffemb.set_thumbnail(url="freefire.png")
 
 
 
